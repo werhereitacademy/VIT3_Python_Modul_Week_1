3f7f4816e247b0530359120e1eac52dd62517c7a
SametKilic <157283341+kilicsamet1986@users.noreply.github.com>
2024-01-22 18:04:44 +0100

    Update samet_week1_solution.py

--- samet_week1_solution.py
+++ samet_week1_solution.py
@@ -1,3 +1,22 @@
-#1 soru
-#Soru 1 : 1'den 10'a kadar olan sayıları ekrana yazdıran bir Python kodu yazınız.
+#Soru 1 : 
 print(*range(10))
+
+#***********************************#
+#Soru 2:
+#version1
+num = int(input("Type een getal:"))
+print(*range(0,num,2)) 
+
+#version2
+num = int(input("Type een getal:"))
+for i in range(0,num,2):
+    print(i,end=" ") 
+#version3
+num = int(input("Type een getal:"))
+i = 0
+while(i < num):
+    if i%2 == 0:
+        print(i,end=" ")
+    i+=1
+  
+#***********************************#
