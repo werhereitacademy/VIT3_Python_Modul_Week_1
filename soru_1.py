e69e28a501dfb2e01cc4afa7ec7cbe6df0613611
Guluzar Coskun <mavis.s.coskun@gmail.com>
2024-01-25 16:05:01 +0100

    soru 1,2,3 cevaplari

---
+++ soru_1.py
@@ -0,0 +1,5 @@
+#Soru 1 : 1'den 10'a kadar olan sayıları ekrana yazdıran bir Python kodu yazınız.
+
+for i in range(1,11):
+    print(i)
+    
---
+++ soru_3.py
@@ -0,0 +1,11 @@
+# Soru 3 : Kullanıcıdan bir başlangıç ve bitiş değeri alan ve bu değerler arasındaki tüm sayıları ekrana yazdıran bir Python kodu yazınız(bitis degeri dahil).
+
+baslangic = int(input("baslangic: "))
+bitis = int(input("bitis: "))
+
+i = baslangic
+
+while i <= bitis:
+    print(i)
+    i += 1
+    
